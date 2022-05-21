601fff05d046b28f0c0b7c5e085f21a8259500e7
Tsveti1103 <cvstoqnova@gmail.com>
2022-05-21 13:00:05 +0300

    Create sumOfNumbers.js

---
+++ JS Advanced/syntaxFunctionsAndStatementsLab/sumOfNumbers.js
@@ -0,0 +1,10 @@
+function slove(one,two){
+    one = Number(one);
+    two = Number(two);
+    let result=0;
+    for(one;one<=two;one++){
+        result+=one
+    }
+    console.log(result)
+}
+slove('-8', '20')
