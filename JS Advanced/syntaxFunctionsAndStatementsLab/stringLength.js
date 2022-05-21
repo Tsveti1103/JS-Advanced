cde1ab8e8fc1cde384277b8060a3a3f47ccaa366
Tsveti1103 <cvstoqnova@gmail.com>
2022-05-21 13:00:07 +0300

    Create stringLength.js

---
+++ JS Advanced/syntaxFunctionsAndStatementsLab/stringLength.js
@@ -0,0 +1,7 @@
+function slove(one,two,three) {
+    totalLenght = one.length + two.length + three.length
+    averageLenght = Math.floor(totalLenght / 3)
+    console.log(totalLenght)
+    console.log(averageLenght)
+}
+slove('chocolate', 'ice cream', 'cake')
