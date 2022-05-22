77aa20d6bca91cd0913831c8ebac57882583bc6a
Tsveti1103 <cvstoqnova@gmail.com>
2022-05-22 15:08:13 +0300

    Create roadRadar.js

---
+++ JS Advanced/syntaxFunctionsAndStatementsExercise/roadRadar.js
@@ -0,0 +1,43 @@
+function slove(speed, area) {
+    let limit
+    switch (area) {
+        case 'motorway':
+            limit = 130
+            break
+
+        case 'interstate':
+            limit = 90
+            break
+
+        case 'city':
+            limit = 50
+            break
+
+        case 'residential':
+            limit = 20
+            break
+    }
+    if (speed <= limit) {
+        console.log(`Driving ${speed} km/h in a ${limit} zone`)
+    }
+    else{
+        let status;
+        let difference = speed - limit;
+        if (difference <=20){
+            status = 'speeding'
+        }
+        else if (difference <=40){
+            status = 'excessive speeding'
+        }
+        else {
+            status = 'reckless driving'
+        }
+        console.log(`The speed is ${difference} km/h faster than the allowed speed of ${limit} - ${status}`)
+        }
+
+
+    }
+    slove(40, 'city');
+    slove(21, 'residential');
+    slove(120, 'interstate');
+    slove(200, 'motorway');
