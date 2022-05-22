ea4a2b108557d07bc0a59d8553f9e8a183e71ac9
Tsveti1103 <cvstoqnova@gmail.com>
2022-05-22 16:14:23 +0300

    Update previousDay.js

--- JS Advanced/syntaxFunctionsAndStatementsExercise/previousDay.js
+++ JS Advanced/syntaxFunctionsAndStatementsExercise/previousDay.js
@@ -1,7 +1,7 @@
-function slove(year, month, day){
+function solve(year, month, day){
     let date = new Date(year, month-1, day);
     date.setDate(date.getDate() -1)
     console.log(`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`)
 }
-slove(2016, 9, 30);
-slove(2016, 10, 1);
+solve(2016, 9, 30);
+solve(2016, 10, 1);
