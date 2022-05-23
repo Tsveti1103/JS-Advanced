b460eb640767276d3aeea8136cb993902e17bb66
Tsveti1103 <cvstoqnova@gmail.com>
2022-05-23 16:13:36 +0300

    Update biggerHalf.js

--- JS Advanced/arraysAndNestedArraysLab/biggerHalf.js
+++ JS Advanced/arraysAndNestedArraysLab/biggerHalf.js
@@ -1,6 +1,6 @@
 function biggerHalf(arr){
     arr.sort((a,b) =>a-b)
     let result = arr.slice(-Math.ceil(arr.length/2))
-    console.log(result)
+    return result
 }
 biggerHalf([3, 19, 14, 7, 2, 19, 6])
