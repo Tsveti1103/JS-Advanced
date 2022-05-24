084ac8a1e64c6a224df2ddd3a7bbf8545a830e7a
Tsveti1103 <cvstoqnova@gmail.com>
2022-05-24 14:49:29 +0300

    Update sortAnArrayBy2Criteria.js

--- JS Advanced/arraysAndNestedArraysExercise/sortAnArrayBy2Criteria.js
+++ JS Advanced/arraysAndNestedArraysExercise/sortAnArrayBy2Criteria.js
@@ -1,16 +1,15 @@
-function sortArr(arr){
-    arr.sort((a,b)=>{
-        if (a.length != b.length){
-            return  a.length - b.length
+function sortArr(arr) {
+    arr.sort((a, b) => {
+        if (a.length != b.length) {
+            return a.length - b.length
         }
         return a.localeCompare(b)
     })
     return arr.join('\n')
 }
-console.log(sortArr(['Isacc', 
-'Theodor', 
-'Jack', 
-'Harrison', 
-'George']
 
-))
+console.log(sortArr(['Isacc',
+    'Theodor',
+    'Jack',
+    'Harrison',
+    'George']))
