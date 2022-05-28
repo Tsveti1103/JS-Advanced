a5fd42a19c56a3b568a861de61f2d998009ed5c0
Tsveti1103 <cvstoqnova@gmail.com>
2022-05-28 20:02:35 +0300

    Update orbit.js

--- arraysAndNestedArraysExercise/orbit.js
+++ arraysAndNestedArraysExercise/orbit.js
@@ -5,17 +5,17 @@ function orbit(arr) {
         matrix.push(Array(columns).fill(0))
     }
     matrix[row][col] = 1
-    for (let r = 0; r<rows; r++) {
-        for(let c = 0; c<columns; c++) {
-            a = Math.max(Math.abs(r-row),Math.abs(c-col))+1
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            a = Math.max(Math.abs(r - row), Math.abs(c - col)) + 1
             matrix[r][c] = a
         }
     }
-   
+
     for (row in matrix) {
         console.log(matrix[row].join(' '))
     }
-    
+
 }
 
 orbit([4, 4, 0, 0])
