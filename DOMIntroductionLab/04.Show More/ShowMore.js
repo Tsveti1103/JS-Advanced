58f9358f4b13875c990b9635f2aeed11fac23633
Tsveti1103 <cvstoqnova@gmail.com>
2022-05-30 16:34:12 +0300

    Create ShowMore.js

---
+++ DOMIntroductionLab/04.Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+   const read = document.getElementById('more')
+   read.style.display = 'none'
+   const text = document.getElementById('text')
+   text.style.display = 'inline'
+}
