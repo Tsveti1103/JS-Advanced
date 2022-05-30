f214e8940bb7a622ecfe1a62c25975cb213cc57c
Tsveti1103 <cvstoqnova@gmail.com>
2022-05-30 16:30:16 +0300

    Create SumNumbers.js

---
+++ DOMIntroductionLab/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,6 @@
+function calc() {
+    let numberOne = document.getElementById('num1').value
+    let numberTwo= document.getElementById('num2').value
+    let sum = document.getElementById('sum')
+    sum.value = Number(numberOne) + Number(numberTwo)
+}
