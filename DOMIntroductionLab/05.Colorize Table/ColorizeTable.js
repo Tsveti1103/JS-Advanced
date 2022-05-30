15edd00f3b26f3031afde1d5a6945ef44576c554
Tsveti1103 <cvstoqnova@gmail.com>
2022-05-30 16:52:28 +0300

    Create ColorizeTable.js

---
+++ DOMIntroductionLab/05.Colorize Table/ColorizeTable.js
@@ -0,0 +1,6 @@
+function colorize() {
+    const rows = Array.from(document.getElementsByTagName('tr'))
+    for(let i = 1; i < rows.length; i+=2){
+        rows[i].style.background = 'teal' 
+    }
+}
