3e118d9adde5e1a089f1232b0a5cb0a719ac2bf6
Tsveti1103 <cvstoqnova@gmail.com>
2022-05-31 14:46:00 +0300

    Create subtract.js

---
+++ DOMIntroductionExercise/01. Subtraction/subtract.js
@@ -0,0 +1,5 @@
+function subtract() {
+    const first = Number(document.getElementById('firstNumber').value);
+    const second = Number(document.getElementById('secondNumber').value);
+    document.getElementById('result').textContent = first - second
+}
