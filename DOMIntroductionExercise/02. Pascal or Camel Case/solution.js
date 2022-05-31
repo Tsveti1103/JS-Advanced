2d5023fe745f684b6740178e81818bcd4d4d4140
Tsveti1103 <cvstoqnova@gmail.com>
2022-05-31 16:06:24 +0300

    Update solution.js

--- DOMIntroductionExercise/02. Pascal or Camel Case/solution.js
+++ DOMIntroductionExercise/02. Pascal or Camel Case/solution.js
@@ -1,23 +1,50 @@
+// function solve() {
+//   let result = ''
+//   let text = document.getElementById('text').value;
+//   text = text.toLowerCase()
+//   let words = text.split(' ')
+//   let textConverter = document.getElementById('naming-convention').value;
+//   if (textConverter == 'Camel Case') {
+//     result += words[0]
+//     for (let i = 1; i < words.length; i++) {
+//       result += words[i][0].toUpperCase() + words[i].substring(1);
+//     }
+//   }
+//   else if (textConverter == 'Pascal Case') {
+//     for (let i = 0; i < words.length; i++) {
+//       result += words[i][0].toUpperCase() + words[i].substring(1);
+//     }
+//   }
+//   else {
+//     result = 'Error!'
+//   }
+
+//   document.getElementById('result').textContent = result
+// } 
+
+
+
+
 function solve() {
   let result = ''
   let text = document.getElementById('text').value;
   text = text.toLowerCase()
   let words = text.split(' ')
   let textConverter = document.getElementById('naming-convention').value;
-  if (textConverter == 'Camel Case') {
-    result += words[0]
-    for (let i = 1; i < words.length; i++) {
-      result += words[i][0].toUpperCase() + words[i].substring(1);
-    }
-  }
-  else if (textConverter == 'Pascal Case') {
-    for (let i = 0; i < words.length; i++) {
-      result += words[i][0].toUpperCase() + words[i].substring(1);
-    }
-  }
-  else {
+
+  if (textConverter != 'Pascal Case' && textConverter != 'Camel Case') {
     result = 'Error!'
   }
+  else {
+    for (let i = 0; i < words.length; i++) {
 
+      result += words[i][0].toUpperCase() + words[i].substring(1);
+    }
+    if (textConverter == 'Camel Case') {
+      result = result[0].toLocaleLowerCase() + result.substring(1);
+    }
+  }
   document.getElementById('result').textContent = result
 } 
+
+
