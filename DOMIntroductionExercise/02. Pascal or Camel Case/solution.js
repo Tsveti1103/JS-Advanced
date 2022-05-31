e62213afeeae64f6185a61bf691d9032fe15fc86
Tsveti1103 <cvstoqnova@gmail.com>
2022-05-31 15:18:32 +0300

    Update solution.js

--- DOMIntroductionExercise/02. Pascal or Camel Case/solution.js
+++ DOMIntroductionExercise/02. Pascal or Camel Case/solution.js
@@ -3,24 +3,21 @@ function solve() {
   let text = document.getElementById('text').value;
   text = text.toLowerCase()
   let words = text.split(' ')
-  let textConverter =  document.getElementById('naming-convention').value;
-  if(textConverter =='Camel Case'){
+  let textConverter = document.getElementById('naming-convention').value;
+  if (textConverter == 'Camel Case') {
     result += words[0]
-    for(let i = 1; i < words.length; i++){
-      words[i].charAt(0).toUpperCase()
-      result+=words[i]
+    for (let i = 1; i < words.length; i++) {
+      result += words[i][0].toUpperCase() + words[i].substring(1);
     }
   }
-  else if(textConverter == 'Pascal Case'){
-    // result += map()
-    for(let i = 0; i < words.length; i++){
-      words[i].charAt(0).toUpperCase()
-      result+= words[i]
+  else if (textConverter == 'Pascal Case') {
+    for (let i = 0; i < words.length; i++) {
+      result += words[i][0].toUpperCase() + words[i].substring(1);
     }
   }
-  else{
+  else {
     result = 'Error!'
   }
 
-   document.getElementById('result').textContent = result
+  document.getElementById('result').textContent = result
 } 
