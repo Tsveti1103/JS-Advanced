5bc2bc50c05f38d0040bf5010a54968ff9e0bc18
Tsveti1103 <cvstoqnova@gmail.com>
2022-06-01 19:04:21 +0300

    Create report.js

---
+++ DOMIntroductionExercise/08. Generate Report/report.js
@@ -0,0 +1,21 @@
+function generateReport() {
+    let headRows = document.querySelectorAll('thead tr th input')
+    let rows = document.querySelectorAll('tbody tr')
+    let result = {}
+    let final = []
+    for (let i = 0; i < rows.length; i++) {
+        let result = {}
+        for (let index = 0; index < headRows.length; index++) {
+            if (headRows[index].checked) {
+                let propertyName = headRows[index].name
+                let propertyValue = rows[i].children[index].textContent
+                result[propertyName] = propertyValue
+                final.push(result)
+            }
+        }
+    }
+
+    document.getElementById('output').value = JSON.stringify(final)
+    data = JSON.parse(document.getElementById('output').value)
+    console.log(data.length)
+}
