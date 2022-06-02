8f5d772f05d08b56bfde3bb41a3fed384ab09bd9
Tsveti1103 <cvstoqnova@gmail.com>
2022-06-02 16:58:19 +0300

    Create app.js

---
+++ DOMManipulationsAndEventsLab/04. Mouse-Gradient/app.js
@@ -0,0 +1,8 @@
+function attachGradientEvents() {
+    const gradient = document.getElementById('gradient');
+    gradient.addEventListener('mousemove', onMouseOver);
+    const result = document.getElementById('result')
+    function onMouseOver(ev) {
+        result.textContent = `${Math.floor(ev.offsetX / gradient.clientWidth * 100)}%`
+    }
+}
