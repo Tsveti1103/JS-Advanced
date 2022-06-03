59656df578b771b071eacc449318e1a759b29cf6
Tsveti1103 <cvstoqnova@gmail.com>
2022-06-03 16:43:40 +0300

    Update app.js

--- DOMManipulationАndEventsExercise/03. Locked-Profile/app.js
+++ DOMManipulationАndEventsExercise/03. Locked-Profile/app.js
@@ -1,20 +1,20 @@
 function lockedProfile() {
     //user1HiddenFields
-    Array.from(document.querySelectorAll('.profile button')).forEach(b=>b.addEventListener('click',show))
+    Array.from(document.querySelectorAll('.profile button')).forEach(b => b.addEventListener('click', show))
 
-    function show(ev){
+    function show(ev) {
         const profile = ev.target.parentElement;
         const isActive = profile.querySelector('input[type="radio"][value = "unlock"]').checked;
-        if(isActive){
-            let div =  profile.querySelector('div');
-            if(ev.target.textContent == 'Show more'){
+        if (isActive) {
+            let div = profile.querySelector('div');
+            if (ev.target.textContent == 'Show more') {
                 div.style.display = 'block';
                 ev.target.textContent = 'Hide it';
-            }else if(ev.target.textContent == 'Hide it') {
+            } else if (ev.target.textContent == 'Hide it') {
                 div.style.display = 'none';
                 ev.target.textContent = 'Show more';
             }
-          
+
         }
     }
 }
