2e8330257e523bc17afad6772f4c438a19c5b4af
Tsveti1103 <cvstoqnova@gmail.com>
2022-06-03 14:32:51 +0300

    Create script.js

---
+++ DOMManipulationАndEventsExercise/01. Sections/script.js
@@ -0,0 +1,16 @@
+function create(words) {
+   let content = document.getElementById('content')
+   for (const word of words) {
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+      p.textContent = word;
+      p.style.display = 'none';
+      div.appendChild(p);
+      div.addEventListener('click', reveal);
+      content.appendChild(div);
+      
+   }
+   function reveal(ev) {
+      ev.currentTarget.children[0].style.display = '';
+   }
+}
