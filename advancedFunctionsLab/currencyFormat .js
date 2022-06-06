3c102cb8b1095cb3e07f81b1afb0a0de76acfdf5
Tsveti1103 <cvstoqnova@gmail.com>
2022-06-06 14:07:52 +0300

    Create currencyFormat .js

---
+++ advancedFunctionsLab/currencyFormat .js
@@ -0,0 +1,18 @@
+function createFormatter(){
+    return currencyFormatter
+}
+
+function currencyFormatter(separator, symbol, symbolFirst, value) {
+    let result = Math.trunc(value) + separator;
+    result += value.toFixed(2).substr(-2,2);
+    if (symbolFirst) return symbol + ' ' + result;
+    else return result + ' ' + symbol;
+    {return (v)=>value(separator, symbol, symbolFirst,v)}
+}
+
+
+
+let dollarFormatter = createFormatter(',', '$', true, currencyFormatter);
+console.log(dollarFormatter(5345));   // $ 5345,00
+console.log(dollarFormatter(3.1429)); // $ 3,14
+console.log(dollarFormatter(2.709));  // $ 2,71
