531097b8fd184bf225f3575977960a95db7361d5
Tsveti1103 <cvstoqnova@gmail.com>
2022-06-07 11:31:47 +0300

    Create functionalSum.js

---
+++ advancedFunctionsExercise/functionalSum.js
@@ -0,0 +1,13 @@
+function add(n){
+    const inner = function(a){
+        n+=a;
+        return inner;
+    };
+    inner.toString = function(){
+        return n;
+    }
+    return inner;
+}
+
+console.log(add(1).toString())
+console.log(add(1)(6)(-3).toString())
