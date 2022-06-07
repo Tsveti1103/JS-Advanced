def6fab9a49855403224b78ce76a92211c13c4b0
Tsveti1103 <cvstoqnova@gmail.com>
2022-06-07 10:35:32 +0300

    Create sortArray.js

---
+++ advancedFunctionsExercise/sortArray.js
@@ -0,0 +1,25 @@
+// function sortArray(arr, command) {
+//     if (command == 'asc') {
+//         arr.sort((a, b) => a - b);
+//     } else {
+//         arr.sort((a, b) => b - a);
+//     }
+//     return arr
+// }
+
+// function sortArray(arr, command) {
+//     const metods = {
+//         asc: (a, b) => a - b,
+//         desc: (a, b) => b - a
+//     }
+//     arr.sort(metods[command]);
+//     return arr
+// }
+function sortArray(arr, command) {
+    return arr.sort((a, b) => (command == 'asc'? a - b: b - a))
+}
+
+
+
+console.log(sortArray([14, 7, 17, 6, 8], 'asc'))
+console.log(sortArray([14, 7, 17, 6, 8], 'desc'))
