cc88058007102caf9fd6c5a0ecf15b62a81235bd
Tsveti1103 <cvstoqnova@gmail.com>
2022-06-09 11:32:05 +0300

    Update playingCards.js

--- unitTestingAndErrorHandlingLab/playingCards.js
+++ unitTestingAndErrorHandlingLab/playingCards.js
@@ -1,17 +1,17 @@
-function playingCards(face,suit){
-    const faces = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
-    const suits = {'S':'\u2660',H:'\u2665',D:'\u2666',C:'\u2663'};
-    if(!faces.includes(face)){
+function playingCards(face, suit) {
+    const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    const suits = { 'S': '\u2660', H: '\u2665', D: '\u2666', C: '\u2663' };
+    if (!faces.includes(face)) {
         throw new Error('Invalid face: ' + face);
     }
-    if(!suits.hasOwnProperty(suit)){
+    if (!suits.hasOwnProperty(suit)) {
         throw new Error('Invalid suit: ' + suit);
 
     }
-    const card ={
+    const card = {
         cardFace: face,
         cardSuit: suits[suit],
-        toString(){
+        toString() {
             return this.cardFace + this.cardSuit
         }
     }
