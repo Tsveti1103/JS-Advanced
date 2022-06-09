45d3d41d75512239b65ce9c0f8962e553849da4e
Tsveti1103 <cvstoqnova@gmail.com>
2022-06-09 16:55:54 +0300

    Create sumOfNumbers.test.js

---
+++ unitTestingAndErrorHandlingLab/04.Sum of Numbers/sumOfNumbers.test.js
@@ -0,0 +1,17 @@
+const { expect } = require('chai');
+const { sum } = require('./sumOfNumbers');
+
+describe('sumOfNumbers', () => {
+    it('work with positive numbers',()=>{
+        expect(sum([2,2,3])).to.equal(7);
+    });
+    it('work with negative numbers',()=>{
+        expect(sum([-2,-2,-3])).to.equal(-7);
+    });
+    it('work with zeros',()=>{
+        expect(sum([0,0,0])).to.equal(0);
+    });
+    it('work with string',()=>{
+        expect(isNaN(sum(['a','b']))).to.equal(true);
+    });
+})
