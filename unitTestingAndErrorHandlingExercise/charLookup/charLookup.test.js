9c655dadcc9c61c4289df265ae8d5af6b9bee2b2
Tsveti1103 <cvstoqnova@gmail.com>
2022-06-10 11:24:01 +0300

    Update charLookup.test.js

--- unitTestingAndErrorHandlingExercise/charLookup/charLookup.test.js
+++ unitTestingAndErrorHandlingExercise/charLookup/charLookup.test.js
@@ -1,22 +1,22 @@
-const {expect} = require('chai');
-const {lookupChar} = require('./charLookup');
+const { expect } = require('chai');
+const { lookupChar } = require('./charLookup');
 
-describe('Tests for lookupChar', () =>{
-    it('first parameter is not a string', ()=>{
-        expect(lookupChar(['a'],5)).to.be.undefined;
+describe('Tests for lookupChar', () => {
+    it('first parameter is not a string', () => {
+        expect(lookupChar(['a'], 5)).to.be.undefined;
     });
-    it('second parameter is not a integer', ()=>{
-        expect(lookupChar('a',5.3)).to.be.undefined;
-        expect(lookupChar('a','5')).to.be.undefined;
-        expect(lookupChar('a','a')).to.be.undefined;
+    it('second parameter is not a integer', () => {
+        expect(lookupChar('a', 5.3)).to.be.undefined;
+        expect(lookupChar('a', '5')).to.be.undefined;
+        expect(lookupChar('a', 'a')).to.be.undefined;
     });
-    it('incorrect index', ()=>{
-        expect(lookupChar('a',-1)).to.equal('Incorrect index');
-        expect(lookupChar('a',5)).to.equal('Incorrect index');
+    it('incorrect index', () => {
+        expect(lookupChar('a', -1)).to.equal('Incorrect index');
+        expect(lookupChar('a', 5)).to.equal('Incorrect index');
     });
-    it('return character', ()=>{
-        expect(lookupChar('a',0)).to.equal('a');
-        expect(lookupChar('abcdef',5)).to.equal('f');
+    it('return character', () => {
+        expect(lookupChar('a', 0)).to.equal('a');
+        expect(lookupChar('abcdef', 5)).to.equal('f');
     });
 
 })
