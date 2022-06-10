cc2046a68f282dcd24af8045f225bb6e84abdecd
Tsveti1103 <cvstoqnova@gmail.com>
2022-06-10 13:13:34 +0300

    Create app.js

---
+++ unitTestingAndErrorHandlingExercise/05.Notification/app.js
@@ -0,0 +1,8 @@
+function notify(message) {
+  let notification = document.getElementById('notification');
+  notification.textContent = message;
+  notification.style.display = 'block';
+  notification.addEventListener('click', (event) => {
+    event.target.style.display = 'none';
+  })
+}
