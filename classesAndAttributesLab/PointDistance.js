d3e3b85edba658c707b83b574582b20747fbf0a8
Tsveti1103 <cvstoqnova@gmail.com>
2022-06-12 10:35:03 +0300

    Update PointDistance.js

--- classesAndAttributesLab/PointDistance.js
+++ classesAndAttributesLab/PointDistance.js
@@ -1,15 +1,15 @@
-class Point{
-    constructor(x,y){
+class Point {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
     }
-    static distance(p1,p2){
-        Math.sqrt((p1.x-p2.x)**2+(p1.y-p2.y)**2)
+    static distance(p1, p2) {
+        return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2)
     }
 }
 
-const point1 = new Point(1,1)
-const point2 = new Point(4,5)
+const point1 = new Point(1, 1)
+const point2 = new Point(4, 5)
 
-console.log(Point.distance(point1,point2))
-console.log(point1,point2)
+console.log(Point.distance(point1, point2))
+console.log(point1, point2)
