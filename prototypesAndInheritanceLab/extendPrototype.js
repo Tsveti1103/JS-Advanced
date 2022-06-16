c282e6f52b292b7e74e9057f3b52d0e09da5c991
Tsveti1103 <cvstoqnova@gmail.com>
2022-06-16 19:00:35 +0300

    Create extendPrototype.js

---
+++ prototypesAndInheritanceLab/extendPrototype.js
@@ -0,0 +1,22 @@
+function extendProrotype(classDef) {
+    classDef.prototype.species = 'Human';
+    classDef.prototype.toSpeciesString = function () {
+        return `I am a ${this.species}. ${this.toString()}`;
+    }
+}
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+
+Person.prototype.toString = function () {
+    return `To string method from prototype of ${this.name}`
+}
+
+
+extendProrotype(Person);
+const p = new Person('Petar', 20);
+console.log(p.toSpeciesString())
+console.log(p.species)
