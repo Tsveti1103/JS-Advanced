0f84bb2457399c2d3a6d6e0ccf32bd0ccb9318b2
Tsveti1103 <cvstoqnova@gmail.com>
2022-06-22 13:19:24 +0300

    Create ChristmasDinner.js

---
+++ examPreparation/JSAdvancedExamPrep15Feb2021/ChristmasDinner.js
@@ -0,0 +1,84 @@
+class ChristmasDinner {
+    constructor(budget) {
+        this.budget = budget;
+        if (this.budget < 0) {
+            throw new Error("The budget cannot be a negative number")
+        }
+        this.dishes = [];
+        this.products = [];
+        this.guests = {};
+    }
+
+    shopping([type, price]) {
+        price = Number(price);
+        if (this.budget < price) {
+            throw new Error("Not enough money to buy this product")
+        }
+        this.products.push(type);
+        this.budget-=price;
+        return `You have successfully bought ${type}!`
+    }
+    recipes(recipe) {
+        let recipeName = recipe.recipeName
+        let productsList = recipe.productsList
+        for (let product of productsList) {
+            if (!this.products.includes(product)) {
+                throw new Error("We do not have this product")
+            }
+        }
+        this.dishes.push({ recipeName, productsList })
+        return `${recipeName} has been successfully cooked!`
+    }
+    inviteGuests(name, dish) {
+
+        if (!(this.dishes.some(recipe => recipe.recipeName == dish))) {
+            throw new Error("We do not have this dish")
+        }
+        if (Object.keys(this.guests).includes(name)) {
+            throw new Error("This guest has already been invited")
+        }
+        this.guests[name] = dish
+        return `You have successfully invited ${name}!`
+    }
+    showAttendance() {
+        let result = []
+        for (let guest in this.guests) {
+            let name = guest
+            let dish = this.guests[guest]
+            let products = (this.dishes.find(d => d.recipeName == dish)).productsList
+            result.push(`${name} will eat ${dish}, which consists of ${products.join(", ")}`)
+        }
+        return result.join("\n");
+    }
+
+}
+
+
+let dinner = new ChristmasDinner(300);
+// dinner.budget = -7
+console.log(dinner.shopping(['Salt', 1]));
+console.log(dinner.shopping(['Beans', 3]));
+console.log(dinner.shopping(['Cabbage', 4]));
+console.log(dinner.shopping(['Rice', 2]));
+console.log(dinner.shopping(['Savory', 1]));
+console.log(dinner.shopping(['Peppers', 1]));
+console.log(dinner.shopping(['Fruits', 40]));
+console.log(dinner.shopping(['Honey', 10]));
+console.log(dinner.recipes({
+    recipeName: 'Oshav',
+    productsList: ['Fruits', 'Honey']
+}))
+console.log(dinner.recipes({
+    recipeName: 'Folded cabbage leaves filled with rice',
+    productsList: ['Cabbage', 'Rice', 'Salt', 'Savory']
+}))
+console.log(dinner.recipes({
+    recipeName: 'Peppers filled with beans',
+    productsList: ['Beans', 'Peppers', 'Salt']
+}))
+console.log(dinner.inviteGuests('Ivan', 'Oshav'))
+console.log(dinner.inviteGuests('Petar', 'Folded cabbage leaves filled with rice'))
+console.log(dinner.inviteGuests('Georgi', 'Peppers filled with beans'))
+console.log(dinner.showAttendance());
+
+
